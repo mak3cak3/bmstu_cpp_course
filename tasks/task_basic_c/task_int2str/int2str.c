f6389cec43f837d795f8a7a091967b3596aeceea
Akhmat <axmat.akbaev@icloud.com>
2024-10-16 20:40:20 +0300

    Revert changes to stock

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,33 +1,8 @@
 #include <assert.h>
-#include <stdio.h>
 #include "int2str.h"
+#include "stdio.h"
 
 char* int2str(int number) {
-
-    int i = 0;
-    int nega = 0;
-    int temp = number;
-    static char buf[16];
-
-    if (number == 0){
-        buf[i] = '0';;
-        return &buf[i];
-    }
-
-    if (number < 0) {
-        nega = 1;
-        number = -number;
-    }
-
-    while (temp > 0) {i++;}
-
-    while (number > 0 && i >= 0){
-        buf[i] = '0' + number % 10;
-        number /= 10;
-        i--;
-    }
-
-    if (nega) buf[i--] = '-';
-
-    return &buf;
+    char* str = "0";
+    return str;
 }
