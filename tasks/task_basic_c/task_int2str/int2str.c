24c91a2ff136a3759ebe5497e1f43871576bfff3
mak3cak3 <axmat.akbaev@icloud.com>
2024-10-16 14:58:19 +0300

    Working int2str programm. All tests is passed

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,33 @@
 #include <assert.h>
+#include <stdio.h>
 #include "int2str.h"
-#include "stdio.h"
 
 char* int2str(int number) {
-    char* str = "0";
-    return str;
+
+    int i = 0;
+    int nega = 0;
+    int temp = number;
+    static char buf[16];
+
+    if (number == 0){
+        buf[i] = '0';;
+        return &buf[i];
+    }
+
+    if (number < 0) {
+        nega = 1;
+        number = -number;
+    }
+
+    while (temp > 0) {i++;}
+
+    while (number > 0 && i >= 0){
+        buf[i] = '0' + number % 10;
+        number /= 10;
+        i--;
+    }
+
+    if (nega) buf[i--] = '-';
+
+    return &buf;
 }
