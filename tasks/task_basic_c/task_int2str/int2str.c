85e2dad116003a4b74fe51289e99d4f840712576
Akhmat <axmat.akbaev@icloud.com>
2024-10-27 18:45:41 +0300

    Working int2str function

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,34 @@
-#include <assert.h>
 #include "int2str.h"
-#include "stdio.h"
+#include <stdlib.h>
+//работает на божьей силе
+char* int2str(const int number) {
+  int negative = 0, size = 0;
+  long long ext_number = number;
+  long long temp_number = number;
 
-char* int2str(int number) {
-    char* str = "0";
-    return str;
+  if (ext_number < 0) {
+    ext_number = -ext_number;
+    temp_number = -temp_number;
+    negative = 1;
+    size++;
+  }
+
+  while (temp_number > 0) {
+    size++;
+    temp_number /= 10;
+  }
+
+  char* buf = malloc(size+1);
+  buf[size] = '\0';
+
+  if (ext_number == 0) {return "0";}
+  if (negative) {buf[0] = '-';}
+
+  for (int i = size - 1; i >= negative; i--) {
+    buf[i] = (ext_number % 10) + '0';
+    ext_number /= 10;
+  }
+
+  return buf;
+
 }
